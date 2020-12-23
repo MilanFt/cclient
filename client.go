a7d0c169109de940089ba969294b221260c67125
MilanFt <m.matok@icloud.com>
2020-12-23 03:02:03 +0100

    Fixes

--- client.go
+++ client.go
@@ -8,18 +8,18 @@ import (
 	utls "github.com/refraction-networking/utls"
 )
 
-func NewClient(clientHello utls.ClientHelloID, jar http.CookieJar, proxyUrl ...string) (http.Client, error) {
+func NewClient(clientHello utls.ClientHelloID, jar http.CookieJar, proxyUrl ...string) (*http.Client, error) {
 	if len(proxyUrl) > 0 && len(proxyUrl) > 0 {
 		dialer, err := newConnectDialer(proxyUrl[0])
 		if err != nil {
-			return http.Client{}, err
+			return &http.Client{}, err
 		}
-		return http.Client{
+		return &http.Client{
 			Transport: newRoundTripper(clientHello, dialer),
 			Jar:       jar,
 		}, nil
 	} else {
-		return http.Client{
+		return &http.Client{
 			Transport: newRoundTripper(clientHello, proxy.Direct),
 			Jar:       jar,
 		}, nil
